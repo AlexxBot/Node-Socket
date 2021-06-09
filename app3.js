b0fa1ff23f1f34f765be16a22afc8c0f20a119f8
AlexxBot <alexx_96_sf17@hotmail.com>
2021-06-08 20:44:06 -0500

    app3

--- app3.js
+++ app3.js
@@ -9,18 +9,22 @@ const io = require("socket.io")(http,{
     }
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 app.get('/', (req, res) => {
+    res.json("hola mundo")
     console.log("aplicacion funciona correctamente");
+    
+
 });
 
 http.listen(app.get('port'), () => {
-    console.log('Escuchando en el puerto 3000');
+    console.log('Escuchando en el puerto 4000');
 });
 
 //sin ecma eccript esto seria function(socket){} end vez de (socket)
 io.on("connection", (socket) => {
+
     console.log("se conecto un cliente");
 
     socket.on("test", (saludo)  => {
@@ -32,5 +36,21 @@ io.on("connection", (socket) => {
         console.log("el servidor recibio un mensaje");
         socket.broadcast.emit("receiveMessage", mensaje);
     });
+    /* let contador = 0;
+    setInterval(() => {
+        console.log()
+        getApiAndEmit(socket, contador)
+        contador++;
+    }, 1000) */
 });
+
+const getApiAndEmit = (socket, contador) => {
+    console.log(contador)
+    socket.broadcast.emit("sendContador", contador)
+}
+
+
+
+
+
 
